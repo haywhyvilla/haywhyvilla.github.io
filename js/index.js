00158214e63d7f8813ffbb5ed96e8411d23db670
Olatunji Ayobami Samson <114535570+haywhyvilla@users.noreply.github.com>
2022-11-05 08:42:54 +0100

    final commit

--- js/index.js
+++ js/index.js
@@ -1,10 +1,28 @@
 import React from "react";
-import ReactDom from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    contact: (
+      <div>
+        <h1>Hello there</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
